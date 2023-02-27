e78220d84a10f9d624ebe91706174d085729c27c
Maksуm Frankov <55510261+MaksymFrankov@users.noreply.github.com>
2023-02-27 17:36:16 +0200

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,19 @@
-import { render } from "@testing-library/react";
+// import { render } from "@testing-library/react";
 import React from "react";
 import { Component } from "react";
 
-class app extends Component {
-  state = {
-   contacts: [],
-   name: '',
-   number: ''
-  }
-};
+export class App extends Component {
+state = {
+  contacts: [
+    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+  ],
+  filter: '',
+  name: '',
+  number: ''
+}
 
 handleChange = event => {
 
@@ -20,14 +25,15 @@ handleChange = event => {
 }
 
 handleSubmit = e => {
-  e.preventDefault();
+e.preventDefault();
   
 }
 
 render() {
   return (
     <form onSubmit={this.handleSubmit}>
-      <label>
+      <label htmlFor="name">
+        Name
         <input
           type="text"
           name="name"
@@ -38,7 +44,27 @@ render() {
           required
       />
       </label>
+      <label htmlFor="number">
+        Number
+      <input
+        type="tel"
+        name="number"
+        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+        required
+      />
+      
+      <ul>
+    {this.state.contacts.map((person) => (
+        <li key={person.id}>{person.name}  {person.number}</li>
+    ))}
+    </ul>
+      
+      </label>
       <button type="submit">Add contact</button>
+      {/* {console.log(this.state.contacts)} */}
     </form>
+    
   )
 }
+};
